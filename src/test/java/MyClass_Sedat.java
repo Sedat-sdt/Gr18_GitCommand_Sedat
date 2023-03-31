70af90db76192380927a21631033247c5d34ffd4
Sedat Maden <madensedat05@gmail.com>
2023-03-31 23:19:29 +0300

    third1 commit

--- src/test/java/MyClass_Sedat.java
+++ src/test/java/MyClass_Sedat.java
@@ -1,5 +1,7 @@
 public class MyClass_Sedat {
     public static void main(String[] args) {
         System.out.println("First commit on my local");
+        System.out.println("second commit on my local");
+
     }
 }
